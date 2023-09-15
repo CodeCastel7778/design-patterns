0dce60674c4bf4a549e284dded924b5a567b1560
Jayanth Kumar <j.ei7778@gmail.com>
2023-09-15 12:38:44 +0530

    Command Pattern

---
+++ src/com/learn/patterns/command/Command.java
@@ -0,0 +1,7 @@
+package com.learn.patterns.command;
+
+public interface Command {
+
+	void execute();
+
+}
---
+++ src/com/learn/patterns/command/Television.java
@@ -0,0 +1,13 @@
+package com.learn.patterns.command;
+
+public class Television {
+
+	public void on() {
+		System.out.println("Television switched on");
+	}
+
+	public void off() {
+		System.out.println("Television switched off");
+	}
+
+}
